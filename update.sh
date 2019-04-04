f9cac007a29bc911d99439c633c0b08c7494d90f
danparisd <43418833+danparisd@users.noreply.github.com>
2019-04-04 10:25:47 -0400

    Create update.sh
    add an update script

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!//bin/bash
+
+sudo systemctl enable NetworkManager.service
+sudo systemctl start NetworkManager.service
+
+read -n 1 -s -r -p "Please connect network to the Internet and press any key when connected"
+
+sudo docker-compose pull web
+
+echo -e "**********| Installation complete. Rebooting... |**********"
+
+sudo reboot now
