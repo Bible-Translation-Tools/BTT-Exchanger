dbfcd5fd8cb5f334b85c3799ecccbbd8f20cf288
danparisd <43418833+danparisd@users.noreply.github.com>
2019-04-05 12:08:37 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,9 +5,15 @@ sudo docker-compose down
 sudo systemctl enable NetworkManager.service
 sudo systemctl start NetworkManager.service
 
-read -n 1 -s -r -p "Please connect network to the Internet and press any key when connected"
+clear
+
+echo -e "\033[1;35mPlease connect network to the Internet and press any key when connected\033[0m"
+
+read -n 1 -s -r 
 
 sudo docker-compose pull web
+
+clear
 
 echo -e "**********| Installation complete. Rebooting... |**********"
 
