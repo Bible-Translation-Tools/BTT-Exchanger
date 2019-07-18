b3015003a775bd9345f977cf638888c1f5bfa97d
danparisd <43418833+danparisd@users.noreply.github.com>
2019-07-18 12:08:20 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,8 +23,8 @@ fi
 echo -e "${COLOR}----------| Installing neccessary software for the server... |----------${NC}"
 
 sudo apt update && sudo apt install -y curl python-pip apt-transport-https ca-certificates software-properties-common jq
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 sudo apt update && sudo apt install -y docker-ce
 sudo pip install docker-compose
 
